328d071684d571aaf97074511d1da08b6a2b1813
wsobczyk <wiktoriasobczyk@gmail.com>
2019-10-30 21:42:23 +0100

    added datepicker

--- Flanki/containers/AddEvent.tsx
+++ Flanki/containers/AddEvent.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 import { NavigationStackProp } from "react-navigation-stack";
-import { Container, Header, Body, Title, Left, Content } from "native-base";
+import {
+  Container,
+  Header,
+  Body,
+  Title,
+  Left,
+  Right,
+  Content,
+  DatePicker
+} from "native-base";
 import { View, Text } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import Input from "../components/input";
@@ -13,7 +22,7 @@ export const AddEvent = ({ navigation }: Props) => {
   const goToMenu = () => {
     navigation.navigate("AuthStack");
   };
-  const emptyFunction = () => { };
+  const emptyFunction = () => {};
   return (
     <Container>
       <Header>
@@ -24,30 +33,57 @@ export const AddEvent = ({ navigation }: Props) => {
           <Title>Stwórz wydarzenie</Title>
         </Body>
       </Header>
-      <Content contentContainerStyle={{ justifyContent: "center", flex: 1, padding: 20 }}>
-       
-            <Input
-              value=""
-              label="Nazwa wydarzenia"
-              onChange={emptyFunction}
-              error="Wypełnij poprawną nazwe"
-              onBlur={emptyFunction}
-            />
-            <Input
-              value=""
-              label="Lokalizacja wydarzenia"
-              onChange={emptyFunction}
-              error="Wypełnij poprawną lokalizacje"
-              onBlur={emptyFunction}
-            />
-            <Input
-              value=""
-              label="Opis wydarzenia"
-              onChange={emptyFunction}
-              error="Wypełnij poprawny opis"
-              onBlur={emptyFunction}
-            />
-          
+      <Content
+        contentContainerStyle={{
+          justifyContent: "center",
+          flex: 1,
+          padding: 20
+        }}
+      >
+        <Input
+          value=""
+          label="Nazwa wydarzenia"
+          onChange={emptyFunction}
+          error="Wypełnij poprawną nazwe"
+          onBlur={emptyFunction}
+        />
+        <Input
+          value=""
+          label="Lokalizacja wydarzenia"
+          onChange={emptyFunction}
+          error="Wypełnij poprawną lokalizacje"
+          onBlur={emptyFunction}
+        />
+        <Input
+          value=""
+          label="Opis wydarzenia"
+          onChange={emptyFunction}
+          error="Wypełnij poprawny opis"
+          onBlur={emptyFunction}
+        />
+
+        <View style={{ flex: 0, flexDirection: "row", alignItems: "center"  }}>
+          <Icon name="calendar" />
+          <Text> Wybierz datę</Text>
+          <Text>                  </Text>
+          <DatePicker
+            
+            defaultDate={new Date(2019, 4, 4)}
+            minimumDate={new Date(2019, 1, 1)}
+            maximumDate={new Date(2019, 12, 31)}
+            locale={"pl"}
+            timeZoneOffsetInMinutes={undefined}
+            modalTransparent={false}
+            animationType={"fade"}
+            androidMode={"spinner"}
+            placeHolderText="  /  /  "
+            textStyle={{ color: "#040404" }}
+            placeHolderTextStyle={{ color: "#EFF0F3",borderBottomColor: "#B4BEC5", backgroundColor: "#B4BEC5"  }}
+            onDateChange={this.setDate}
+            disabled={false}
+          />
+        </View>
+
         {/* <View
           style={{
             flex: 1,

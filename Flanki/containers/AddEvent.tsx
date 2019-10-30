cb02d337049c689a2c1c70afcaea7fab2053a125
wsobczyk <wiktoriasobczyk@gmail.com>
2019-10-30 20:22:34 +0100

    add basic fields

--- Flanki/containers/AddEvent.tsx
+++ Flanki/containers/AddEvent.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import { NavigationStackProp } from "react-navigation-stack";
 import { Container, Header, Body, Title, Left, Content } from "native-base";
-import { View, TextInput, Text } from "react-native";
-import { Input } from "react-native-elements";
+import { View, Text } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+import Input from "../components/input";
 
 type Props = {
   navigation: NavigationStackProp;
 };
 
-export const AddEvent = ({ navigation }) => {
+export const AddEvent = ({ navigation }: Props) => {
   const goToMenu = () => {
     navigation.navigate("AuthStack");
   };
+  const emptyFunction = () => { };
   return (
     <Container>
       <Header>
@@ -23,19 +24,49 @@ export const AddEvent = ({ navigation }) => {
           <Title>Stwórz wydarzenie</Title>
         </Body>
       </Header>
-      <Content>
-        <View
+      <Content contentContainerStyle={{ justifyContent: "center", flex: 1, padding: 20 }}>
+       
+            <Input
+              value=""
+              label="Nazwa wydarzenia"
+              onChange={emptyFunction}
+              error="Wypełnij poprawną nazwe"
+              onBlur={emptyFunction}
+            />
+            <Input
+              value=""
+              label="Lokalizacja wydarzenia"
+              onChange={emptyFunction}
+              error="Wypełnij poprawną lokalizacje"
+              onBlur={emptyFunction}
+            />
+            <Input
+              value=""
+              label="Opis wydarzenia"
+              onChange={emptyFunction}
+              error="Wypełnij poprawny opis"
+              onBlur={emptyFunction}
+            />
+          
+        {/* <View
           style={{
             flex: 1,
             justifyContent: "center",
-            alignItems: "center",
+            alignItems: "center"
           }}
-        > 
-          <View style={{ flex: 10, width:100 }}>
-            <TextInput placeholder="Password" />
-            <Icon name="user" size={24} color="black" />
+        >
+          <View style={{ width: 100, height: 50 }}>
+            <Icon name="user" size={24} color="black" style={{ width: 40 }} />
+            <Input 
+              value="XD"
+              label="Nazwa wydarzenia"
+              onChange={goToMenu}
+              error="xd"
+              onBlur={goToMenu}
+            />
           </View>
-        </View>
+          
+        </View> */}
       </Content>
     </Container>
   );

fccb6d83bf8ae9eb699cac77ff33653923d0d593
wsobczyk <wiktoriasobczyk@gmail.com>
2019-10-30 18:07:34 +0100

    added back to another view

--- Flanki/containers/AddEvent.tsx
+++ Flanki/containers/AddEvent.tsx
@@ -5,21 +5,45 @@ import {
   Header,
   Body,
   Title,
+  Left,
+  Button,
+  Right,
+  
 } from "native-base";
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 type Props = {
     navigation: NavigationStackProp;
 };
+
   
 export const AddEvent = ({ navigation }) => {
+  const goToMenu = () => {
+    navigation.navigate("AuthStack");
+   
+  };
   return (
     <Container>
       <Header>
+      <Left>
+        
+  
+        <Icon
+          name="arrow-left"
+          size={15}
+            color="white"
+            onPress={goToMenu}
+        />
+      
+      
+            </Left>
         <Body>
           <Title>Stwórz wydarzenie</Title>
         </Body>
-        
+    
       </Header>
+     
       
     </Container>);
 };

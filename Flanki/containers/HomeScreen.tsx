99bf3750d13d7c1f242c6d5a3276def72fcb954d
msniec <michal.sniec@gmail.com>
2019-10-31 08:19:20 +0100

    Fix issues

--- Flanki/containers/HomeScreen.tsx
+++ Flanki/containers/HomeScreen.tsx
@@ -1,103 +1,113 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import {
-    Container,
-    Content,
-    Text,
-    Header,
-    Body,
-    Right,
-    Button,
-    Title,
-    Thumbnail,
-    Spinner, Icon, Left
+  Container,
+  Content,
+  Text,
+  Header,
+  Body,
+  Right,
+  Button,
+  Title,
+  Thumbnail,
+  Spinner,
+  Icon,
+  Left,
+  Toast
 } from "native-base";
-import {NavigationStackProp} from "react-navigation-stack";
-import {View} from "react-native";
+import { NavigationStackProp } from "react-navigation-stack";
+import { View } from "react-native";
 import api from "../utils/api";
-import {UserInfoPayload} from "../typings";
-import {ApiResponse} from "apisauce";
-import {DrawerActions} from "react-navigation-drawer";
+import { UserInfoPayload } from "../typings";
+import { ApiResponse } from "apisauce";
+import { DrawerActions } from "react-navigation-drawer";
 
 type Props = {
-    navigation: NavigationStackProp;
+  navigation: NavigationStackProp;
 };
 
 const uri = "https://facebook.github.io/react-native/docs/assets/favicon.png";
 
 const starUri =
-    "https://cdn2.iconfinder.com/data/icons/modifiers-add-on-1-flat/48/Mod_Add-On_1-35-512.png";
+  "https://cdn2.iconfinder.com/data/icons/modifiers-add-on-1-flat/48/Mod_Add-On_1-35-512.png";
 
-export const HomeScreen = ({navigation}: Props) => {
-    const [userName, setUserName] = useState("");
-    useEffect(() => {
-        api.getUserInfo().then(({data}: ApiResponse<UserInfoPayload>) => {
-            setUserName(data.username);
+export const HomeScreen = ({ navigation }: Props) => {
+  const [userName, setUserName] = useState("");
+  useEffect(() => {
+    api.getUserInfo().then(({ data, ok }: ApiResponse<UserInfoPayload>) => {
+      if (ok && data && data.username) {
+        setUserName(data.username);
+      } else {
+        Toast.show({
+          type: "danger",
+          //@ts-ignore
+          text: "You need to login to access this page",
+          buttonText: "Ok"
         });
-    }, []);
+        logout();
+      }
+    });
+  }, []);
 
-    const logout = () => {
-        navigation.navigate("AuthStack");
-        api.logout();
-    };
+  const logout = () => {
+    navigation.navigate("AuthStack");
+    api.logout();
+  };
 
-    return (
-        <Container>
-            <Header>
-                <Left>
-                    <Button
-                        transparent
-                        onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-                    >
-                        <Icon name="menu"/>
-                    </Button>
-                </Left>
-                <Body>
-                    <Title>Profil użytkownika</Title>
-                </Body>
-                <Right>
-                    <Button onPress={logout} hasText transparent>
-                        <Text>Wyloguj się</Text>
-                    </Button>
-                </Right>
-            </Header>
-            <Content
-                contentContainerStyle={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flex: 1,
-                    padding: 20
-                }}
+  return (
+    <Container>
+      <Header>
+        <Left>
+          <Button transparent onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+            <Icon name="menu" />
+          </Button>
+        </Left>
+        <Body>
+          <Title>Profil użytkownika</Title>
+        </Body>
+        <Right>
+          <Button onPress={logout} hasText transparent>
+            <Text>Wyloguj się</Text>
+          </Button>
+        </Right>
+      </Header>
+      <Content
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+          flex: 1,
+          padding: 20
+        }}
+      >
+        {userName.length === 0 && <Spinner />}
+        {userName.length > 0 && (
+          <>
+            <Thumbnail large source={{ uri }} />
+            <Text style={{ marginTop: 10 }}>{userName}</Text>
+            <Text style={{ marginTop: 10 }}>Punkty: 1234</Text>
+            <View
+              style={{
+                marginTop: 10,
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
             >
-                {userName.length === 0 && <Spinner/>}
-                {userName.length > 0 && (
-                    <>
-                        <Thumbnail large source={{uri}}/>
-                        <Text style={{marginTop: 10}}>{userName}</Text>
-                        <Text style={{marginTop: 10}}>Punkty: 1234</Text>
-                        <View
-                            style={{
-                                marginTop: 10,
-                                flexDirection: "row",
-                                justifyContent: "center",
-                                alignItems: "center"
-                            }}
-                        >
-                            <Text>Opinia: </Text>
-                            <Thumbnail small source={{uri: starUri}}/>
-                            <Thumbnail small source={{uri: starUri}}/>
-                            <Thumbnail small source={{uri: starUri}}/>
-                            <Thumbnail small source={{uri: starUri}}/>
-                            <Thumbnail small source={{uri: starUri}}/>
-                        </View>
-                    </>
-                )}
-            </Content>
-        </Container>
-    );
+              <Text>Opinia: </Text>
+              <Thumbnail small source={{ uri: starUri }} />
+              <Thumbnail small source={{ uri: starUri }} />
+              <Thumbnail small source={{ uri: starUri }} />
+              <Thumbnail small source={{ uri: starUri }} />
+              <Thumbnail small source={{ uri: starUri }} />
+            </View>
+          </>
+        )}
+      </Content>
+    </Container>
+  );
 };
 
 HomeScreen.navigationOptions = {
-    header: null
+  header: null
 };
 
 export default HomeScreen;
--- Flanki/containers/Login.tsx
+++ Flanki/containers/Login.tsx
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 import { NavigationStackProp } from "react-navigation-stack";
-import {Container, Content, Form, Button, Text, Toast, Header, Left, Icon, Title, Body, Right} from "native-base";
-import { Formik } from 'formik'
-import Input from '../components/input'
-import * as Yup from 'yup'
+import {
+  Container,
+  Content,
+  Form,
+  Button,
+  Text,
+  Toast,
+  Header,
+  Left,
+  Icon,
+  Title,
+  Body,
+  Right
+} from "native-base";
+import { Formik } from "formik";
+import Input from "../components/input";
+import * as Yup from "yup";
 import api from "../utils/api";
-import {DrawerActions} from "react-navigation-drawer";
+import { DrawerActions } from "react-navigation-drawer";
 
 type Props = {
   navigation: NavigationStackProp;
 };
 
 const validationSchema = Yup.object().shape({
-  username: Yup.string().required('Fill usedrname'),
-  password: Yup.string().required('Fill password')
-})
-
+  username: Yup.string().required("Fill usedrname"),
+  password: Yup.string().required("Fill password")
+});
 
 export const Login = ({ navigation }: Props) => {
-  const initialValues = { username: '', password: '' }
+  const initialValues = { username: "", password: "" };
 
   const handleSubmit = ({ username, password }) => {
     api
@@ -47,10 +59,7 @@ export const Login = ({ navigation }: Props) => {
     <Container>
       <Header>
         <Left>
-          <Button
-              transparent
-              onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-          >
+          <Button transparent onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
             <Icon name="menu" />
           </Button>
         </Left>
@@ -60,7 +69,11 @@ export const Login = ({ navigation }: Props) => {
       </Header>
       <Content contentContainerStyle={{ justifyContent: "center", flex: 1, padding: 20 }}>
         <Form>
-          <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+          >
             {({
               values: { username, password },
               errors,
@@ -70,26 +83,28 @@ export const Login = ({ navigation }: Props) => {
               handleBlur,
               isSubmitting
             }) => {
-              return <>
-                <Input
-                  value={username}
-                  label="Username"
-                  onChange={handleChange('username')}
-                  error={touched.username && (errors.username as string)}
-                  onBlur={handleBlur('username')}
-                />
-                <Input
-                  value={password}
-                  isSecure
-                  label="Password"
-                  onChange={handleChange('password')}
-                  onBlur={handleBlur('password')}
-                  error={touched.password && (errors.password as string)}
-                />
-                <Button onPress={handleSubmit} block style={{ marginTop: 10 }}>
-                  <Text>Login</Text>
-                </Button>
-              </>
+              return (
+                <>
+                  <Input
+                    value={username}
+                    label="Username"
+                    onChange={handleChange("username")}
+                    error={touched.username && (errors.username as string)}
+                    onBlur={handleBlur("username")}
+                  />
+                  <Input
+                    value={password}
+                    isSecure
+                    label="Password"
+                    onChange={handleChange("password")}
+                    onBlur={handleBlur("password")}
+                    error={touched.password && (errors.password as string)}
+                  />
+                  <Button onPress={handleSubmit} block style={{ marginTop: 10 }}>
+                    <Text>Login</Text>
+                  </Button>
+                </>
+              );
             }}
           </Formik>
 

1ae59a58fcc810eb33f183a3022d9937d992e27c
msniec <michal.sniec@gmail.com>
2019-11-05 13:32:12 +0100

    Add map inttegration

--- App/containers/TempPageScreen/index.tsx
+++ App/containers/TempPageScreen/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+
 import {
   Body,
   Button,
@@ -9,6 +11,7 @@ import {
   Icon,
   Left,
   Title,
+  Text,
 } from 'native-base';
 import {DrawerActions} from 'react-navigation-drawer';
 import {NavigationStackProp} from 'react-navigation-stack';
@@ -18,6 +21,10 @@ type Props = {
 };
 
 const TempPage = ({navigation}: Props) => {
+  const [positionData, setPositionData] = useState({
+    latitude: 51.7833,
+    longitude: 19.4667,
+  });
   return (
     <Container>
       <Header>
@@ -32,8 +39,56 @@ const TempPage = ({navigation}: Props) => {
           <Title>In progress</Title>
         </Body>
       </Header>
-      <Content>
-        <Text>In progress</Text>
+      <Content
+        contentContainerStyle={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+        }}>
+        <MapView
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+          showsUserLocation
+          provider={PROVIDER_GOOGLE}
+          onRegionChange={({longitude, latitude}) => {
+            setPositionData({
+              longitude,
+              latitude,
+            });
+          }}
+          initialRegion={{
+            latitude: positionData.latitude,
+            longitude: positionData.longitude,
+            latitudeDelta: 0.0022,
+            longitudeDelta: 0.0021,
+          }}>
+          <Marker
+            coordinate={{
+              latitude: positionData.latitude,
+              longitude: positionData.longitude,
+            }}
+            title={'test'}
+            description={'test'}
+          />
+        </MapView>
+        <Button
+          dark
+          style={{
+            position: 'absolute',
+            top: 10,
+            left: 10,
+          }}>
+          <Text>Accept</Text>
+        </Button>
       </Content>
     </Container>
   );
